b43de93de01f0c690f6434e3675c5907cb669156
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2018-09-02 07:22:44 +0200

    modified table setup to include role

---
+++ app/includes/pages/footer_.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kaysiz
+ * Date: 1/9/2018
+ * Time: 19:52
+ */
---
+++ app/includes/user.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kaysiz
+ * Date: 1/9/2018
+ * Time: 12:04
+ */
+
+class User
+{
+
+}
